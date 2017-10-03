97720ac4a446d5be6a6439b1658e9cec1fe2f5b6
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 17:32:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,13 @@ def take_a_number(queue, new_name)
 end
 
 def line(queue)
-  output_string = "The line is currently:"
-  queue.each_with_index do |person, place|
-    output_string << " #{place}. #{person}"
+  if queue.length > 0
+    output_string = "The line is currently:"
+    queue.each_with_index do |person, place|
+      output_string << " #{place + 1}. #{person}"
+    end
+  else
+    output_string = "Nobody in the queue."
   end
   return output_string
 end
