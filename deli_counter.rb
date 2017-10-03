11be29898b0c2f94a692c8c3085558fc8de57b5b
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 17:09:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 katz_deli = []
 
-def take_a_number(queue, new_name) 
+def take_a_number(queue, new_name)
   queue.push(new_name)
   puts "Welcome, #{new_name}. You are number #{queue.length} in line."
+end
+
+def line(queue)
+  output_string = "The line is currently: "
+  queue.each_with_index do |person, place|
+    output_string << place + ". " + person + " "
 end
